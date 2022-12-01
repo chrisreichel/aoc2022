01399e436cdec0b72a6293eb5988e9dd22184c0a
Christian Reichel <ChristianReichel@fico.com>
2022-12-01 14:03:58 -0600

    resolved day 01

---
+++ 01/elf.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const allFileContents = fs.readFileSync("input.txt", 'utf8');
+
+let elves = []
+
+var biggest = 0;
+var temp_acc = 0;
+var index = 1;
+
+allFileContents.split(/\r?\n/).forEach(line =>  {
+    
+    if(line == "" || line == null || line == undefined){
+        elves.push({"index": index, "calories": temp_acc})
+        index++
+        temp_acc = 0
+    }
+    else {
+        temp_acc += parseInt(line);
+    }
+
+});
+
+let sortedElves = elves.sort((e1, e2) => (e1.calories < e2.calories) ? 1 : (e1.calories > e2.calories) ? -1 : 0);
+
+console.log(sortedElves[0].calories + sortedElves[1].calories + sortedElves[2].calories)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aoc2022
+
+01 -> https://adventofcode.com/2022/day/1
