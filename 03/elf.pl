4cb1477f093dce6ea80329f4869919a09c0a1714
Christian Reichel <ChristianReichel@fico.com>
2022-12-03 09:13:19 -0600

    small refactoring to facilitate part 2 of the challenge

--- 03/elf.pl
+++ 03/elf.pl
@@ -24,31 +24,41 @@ my $filename = 'sample.txt';
 
 open(FH, '<', $filename) or die $!;
 
-my $sum_of_priorities = 0;
+sub calculate_findings{
 
-while(<FH>){
-    my $line = $_;
-    chomp $line;
-    my $line_size = length $line;
+    my ($searcheable,$on_string_content) = @_;
 
-    my $first_part = substr $line, 0, ($line_size/2);
-    my $second_part = substr $line, ($line_size/2), $line_size;
+    my $sum_of_priorities = 0;
 
-    my @first_part_letters = split //, $first_part;
-    my @second_part_letters = split //, $second_part;
+    my @searcheable_letters = split //, $searcheable;
+    my @content = split //, $on_string_content;
 
     my %found;
-    for(@first_part_letters){
+    for(@searcheable_letters){
         my $letter = $_;
-        if ( !$found{$letter} && grep( /^$letter$/, @second_part_letters ) ) {
+        if ( !$found{$letter} && grep( /^$letter$/, @content ) ) {
             my $p = $letter_values{$letter};
             $sum_of_priorities += $p;
             $found{$letter} = 1;
             #print "found $letter with value $p on ";
         }
     }
+    return $sum_of_priorities;
+}
 
-    #print("line $line size of $line_size - parts: $first_part and $second_part \n");
+
+my $sum_of_priorities=0;
+while(<FH>){
+    my $line = $_;
+    chomp $line;
+    my $line_size = length $line;
+
+    my $first_half = substr $line, 0, ($line_size/2);
+    my $second_half = substr $line, ($line_size/2), $line_size;
+
+    $sum_of_priorities += calculate_findings($first_half, $second_half);
+
+    #print("line $line size of $line_size - parts: $first_half and $second_half - priorities $sum_of_priorities\n");
 
 }
 close(FH);
