6235b3afdffb9a295b24afb1816e58564a9287f2
Christian Reichel <ChristianReichel@fico.com>
2022-12-03 08:54:54 -0600

    day 3 first part ok

--- 03/elf.pl
+++ 03/elf.pl
@@ -5,7 +5,7 @@ use warnings;
 
 my %letter_values;
 
-#my @ascii_letterss = ((65..90),(97..122));
+#these are the ASCII intervals for a-z A-Z
 for(((65..90),(97..122))){
     my $ascii_val = $_;
     my $char = chr($ascii_val);
@@ -20,33 +20,15 @@ for(((65..90),(97..122))){
     #print("ASCII $ascii_val is char: $char with elf priority $priority\n");
 }
 
-while ( my ($k,$v) = each %letter_values ) {
-    print "$k => $v\n";
-}
-
-#for(@ascii_num_list_uc){
-#    my $char_num = $_;
-#    my $char = chr($char_num);
-##    my $num  = $char_num - 64;
-#    print("AsciimUC: NUM: $char_num and char: $char - $num \n");
-#}
-
-#my @ascii_num_list_lc = (97..122);
-#for(@ascii_num_list_lc){
-#    my $char_num = $_;
-#    my $char = chr($char_num);
-#    my $num  = $char_num - 70;
-#    print("Ascii LC: NUM: $char_num and char: $char - $num\n");
-#}
-
-
 my $filename = 'sample.txt';
 
 open(FH, '<', $filename) or die $!;
 
+my $sum_of_priorities = 0;
+
 while(<FH>){
     my $line = $_;
-    chomp $line ;
+    chomp $line;
     my $line_size = length $line;
 
     my $first_part = substr $line, 0, ($line_size/2);
@@ -54,15 +36,23 @@ while(<FH>){
 
     my @first_part_letters = split //, $first_part;
     my @second_part_letters = split //, $second_part;
+
+    my %found;
     for(@first_part_letters){
         my $letter = $_;
-        if ( grep( /^$letter$/, @second_part_letters ) ) {
-            print "found $letter on ";
+        if ( !$found{$letter} && grep( /^$letter$/, @second_part_letters ) ) {
+            my $p = $letter_values{$letter};
+            $sum_of_priorities += $p;
+            $found{$letter} = 1;
+            #print "found $letter with value $p on ";
         }
     }
 
-    print("Line $line size of $line_size - parts: $first_part and $second_part \n");
+    #print("line $line size of $line_size - parts: $first_part and $second_part \n");
 
 }
-
 close(FH);
+
+print "First part $sum_of_priorities\n";
+
+
