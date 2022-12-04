033242b0fe8de44a9ef25f302f7f093141b6163a
Christian Reichel <ChristianReichel@fico.com>
2022-12-04 15:47:57 -0600

    Day 4 done

--- Day04/src/main/java/br/net/reichel/Elf.java
+++ Day04/src/main/java/br/net/reichel/Elf.java
@@ -1,19 +1,40 @@
 package br.net.reichel;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Elf {
 
-    public void doSomething() {
+    private static final String REGEX = "(\\d+)-(\\d+),(\\d+)-(\\d+)";
+
+    public OverlappingSectionResponse processFilePart1(String fileName) {
         BufferedReader objReader = null;
+        int numOfFullOverlaps = 0;
+        int numOfPartialOverlaps = 0;
         try {
             String strCurrentLine;
-            objReader = new BufferedReader(new FileReader("sample.txt"));
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            File file = new File(classloader.getResource(fileName).getFile());
+            objReader = new BufferedReader(new FileReader(file));
             while ((strCurrentLine = objReader.readLine()) != null) {
-                System.out.println(strCurrentLine);
+
+                final String[] tokens = parseLine(strCurrentLine);
+                final Range<Integer> leftElfRange = generateRange(tokens[0], tokens[1]);
+                final Range<Integer> rightElfRange = generateRange(tokens[2], tokens[3]);
+
+                if(hasFullOverlap(leftElfRange, rightElfRange)){
+                    numOfFullOverlaps++;
+                }
+                if(hasPartialOverlap(leftElfRange, rightElfRange)){
+                    numOfPartialOverlaps++;
+                }
             }
+            System.out.println("Full overlaps: "+ numOfFullOverlaps + " and partial overlaps: " + numOfPartialOverlaps);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -25,6 +46,59 @@ public class Elf {
                 ex.printStackTrace();
             }
         }
+        return new OverlappingSectionResponse(numOfFullOverlaps, numOfPartialOverlaps);
     }
 
+    public String[] parseLine(String line){
+        final String[] tokens = new String[4];
+        final Pattern pattern = Pattern.compile(REGEX);
+        final Matcher matcher = pattern.matcher(line);
+        if (matcher.find( )) {
+            for(int i=0; i<tokens.length; i++){
+                tokens[i] = matcher.group(i + 1);
+            }
+        } else {
+            System.out.println("NO MATCH");
+        }
+        return tokens;
+    }
+
+    public Range<Integer> generateRange(String lower, String upper){
+        return generateRange(Integer.parseInt(lower), Integer.parseInt(upper));
+    }
+
+    public Range<Integer> generateRange(Integer lower, Integer upper){
+        return  Range.closed(lower, upper);
+    }
+
+    public boolean hasFullOverlap(Range<Integer> firstElfRange, Range<Integer> secondElfRange) {
+        return firstElfRange.encloses(secondElfRange) || secondElfRange.encloses(firstElfRange);
+    }
+
+    public boolean hasPartialOverlap(Range<Integer> firstElfRange, Range<Integer> secondElfRange) {
+        final RangeSet<Integer> firstElfSet = TreeRangeSet.create();
+        firstElfSet.add(firstElfRange);
+        final RangeSet<Integer> secondElfSet = TreeRangeSet.create();
+        secondElfSet.add(secondElfRange);
+
+        return firstElfSet.intersects(secondElfRange) || secondElfSet.intersects(firstElfRange);
+    }
+
+    public class OverlappingSectionResponse {
+        private Integer fullOverlapping;
+        private Integer partialOverlapping;
+
+        public OverlappingSectionResponse(Integer fullOverlapping, Integer partialOverlapping) {
+            this.fullOverlapping = fullOverlapping;
+            this.partialOverlapping = partialOverlapping;
+        }
+
+        public Integer getFullOverlapping() {
+            return fullOverlapping;
+        }
+
+        public Integer getPartialOverlapping() {
+            return partialOverlapping;
+        }
+    }
 }
